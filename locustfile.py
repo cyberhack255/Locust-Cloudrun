4ec7768da5b0e799d93e2ad87ad118e94785a292
skiao-fastly <108320891+skiao-fastly@users.noreply.github.com>
2025-12-24 12:54:58 +0000

    modified file

--- locustfile.py
+++ locustfile.py
@@ -6,8 +6,3 @@ class QuickstartUser(HttpUser):
  @task
  def mainPage(self):
   self.client.get("/")
-
-class WebsiteUser(HttpUser):
-    tasks = [UserBehavior]
-    min_wait = 5000
-    max_wait = 9000
